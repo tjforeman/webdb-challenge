81b0663a021430ff19a223541e470d1af191ba3d
tjforeman <tylerforeman92@gmail.com>
2019-05-03 13:15:43 -0400

    finished crud opperations on both routes

--- routes/actions-route.js
+++ routes/actions-route.js
@@ -21,9 +21,62 @@ router.get('/', (req, res) => {
     db('actions')
     .then(action=>{
       res.status(200).json(action)
-    }).catch(err =>{
+    })
+    .catch(err =>{
         res.status(500).json({error:err,message:'Unable to get the Actions at this time.'})
     })
   });
+
+  router.get('/:id', (req, res) => {
+    db('actions')
+    .where({id:req.params.id})
+    .first()
+    .then(action=>{
+      if(action){
+      res.status(200).json(action)
+      }else{
+        res.status(404).json({message:'the specified Action does not exist'})
+      }
+    })
+    .catch(err =>{
+      res.status(500).json(err)
+    })
+  });
+
+  router.put('/:id', (req, res) => {
+    if (!req.body.description || !req.body.notes || !req.body.project_id){
+        res.status(400).json({message:'Description, notes, and project id are all required to update an Action'})
+    }else{
+      db('actions')
+      .where({id:req.params.id})
+      .update(req.body)
+      .then(count=>{
+      if (count>0) {
+        res.status(200).json({message:`${count} record was updated`})
+      }else{
+        res.status(404).json({message:'The specified Action does not exist'})
+      }
+    })
+    .catch(err =>{
+      res.status(500).json({error:err,message:'Unable to update the Action at this time'})
+    })
+}
+  });
+  
+  router.delete('/:id', (req, res) => {
+    db('actions')
+    .where({id:req.params.id})
+    .del()
+    .then(count =>{
+        if (count>0){
+        res.status(200).json({message:`${count} Action was deleted`})
+        }else{
+        res.status(400).json({message:'the specified Action does not exist'})
+          }
+        })
+    .catch(err =>{
+        res.status(500).json(err)
+        })
+      });
 
 module.exports=router
--- routes/projects-route.js
+++ routes/projects-route.js
@@ -47,5 +47,40 @@ router.get('/', (req, res) => {
       })
   })
 
+  router.put('/:id', (req, res) => {
+    if (!req.body.project_name){
+        res.status(400).json({message:'A name, is required to update an Project'})
+    }else{
+      db('projects')
+      .where({id:req.params.id})
+      .update(req.body)
+      .then(count=>{
+      if (count>0) {
+        res.status(200).json({message:`${count} record was updated`})
+      }else{
+        res.status(404).json({message:'The specified Project does not exist'})
+      }
+    })
+    .catch(err =>{
+      res.status(500).json({error:err,message:'Unable to update the Project at this time'})
+    })
+}
+  });
+      router.delete('/:id', (req, res) => {
+        db('projects')
+        .where({id:req.params.id})
+        .del()
+        .then(count =>{
+            if (count>0){
+            res.status(200).json({message:`${count} Project was deleted`})
+            }else{
+            res.status(400).json({message:'the specified Project does not exist'})
+              }
+            })
+        .catch(err =>{
+            res.status(500).json({error:err,message:'Unabled to delete the Project at this time.'})
+            })
+          });
+
 
 module.exports=router
